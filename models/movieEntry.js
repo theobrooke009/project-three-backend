f82d16103679c2d3c72ec089021c7f73615984c9
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-08-22 13:27:39 +0100

    updated schema

--- models/movieEntry.js
+++ models/movieEntry.js
@@ -10,14 +10,21 @@ const commentSchema = new mongoose.Schema({
 
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true, unique: true, trim: true },
-  decade: { type: String },
+  year: { type: Number, required: true },
+  rated: { type: String, required: false },
+  released: { type: String, required: false },
   runtime: { type: Number, required: true },
   genre: { type: String, required: true },
   director: { type: String, required: true },
+  writer: { type: String, required: false },
+  actors: { type: String, required: true },
   plot: { type: String, maxlength: 500, trim: true },
+  language: { type: String, required: false },
+  country: { type: String, required: false },
+  awards: { type: String, required: false },
   poster: { type: String, required: true },
-  rating: { type: Number, required: false },
-  tagline: { type: String, maxlength: 200 },
+  metascore: { type: Number, required: false },
+  imdbRating: { type: Number, required: true },
   comments: [commentSchema],
 })
 
