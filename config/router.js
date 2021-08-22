22381e1d6d6462c0ff5d4e8f10cb37c752f259f7
Theo Brooke <theobrooke009@gmail.com>
2021-08-22 18:02:37 +0100

    api routing fixed

--- config/router.js
+++ config/router.js
@@ -6,10 +6,10 @@ import user from '../controllers/users.js'
 
 const router = express.Router()
 
-router.route('/api/movies')
+router.route('/movies')
   .get(allMovies.index)
 
-router.route('/api/movies/:movieId')
+router.route('/movies/:movieId')
   .get(allMovies.show)
 
 router.route('/register')
