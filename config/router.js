7195852ff803886f4c32e669231c3531304be6e0
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-08-25 09:58:27 +0100

    wed-morn

--- config/router.js
+++ config/router.js
@@ -2,7 +2,7 @@ import express from 'express'
 import auth from '../controllers/auth.js'
 import allMovies from '../controllers/movies.js'
 import user from '../controllers/users.js'
-import secureRoute from '../lib/secureRoute.js'
+// import secureRoute from '../lib/secureRoute.js'
 
 const router = express.Router()
 
@@ -10,11 +10,11 @@ const router = express.Router()
 //routes removed here 
 
 router.route('/movies')
-  .get(secureRoute, allMovies.index)
+  .get(allMovies.index)
 
 
 router.route('/movies/:movieId')
-  .get(secureRoute, allMovies.show)
+  .get(allMovies.show)
 
 router.route('/register')
   .post(auth.register)
@@ -23,9 +23,9 @@ router.route('/login')
   .post(auth.login)
 
 router.route('/user/:userId')
-  .get(secureRoute, user.show)
+  .get(user.show)
 
 router.route('/marathon')
-  .get(secureRoute, allMovies.index)
+  .get(allMovies.index)
 
 export default router
